021ffc6eb33095011f8ba36ccd2e797cc0e5316b
bowersjuan <bowersjuan@gmail.com>
2023-01-05 12:34:07 -0500

    import react-router-dom and create routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Common/Navbar";
+import Home from "./Pages/Home";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
       <h1>Pursuit App Template</h1>
     </div>
   );
