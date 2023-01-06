b65d7c105e8bbcc37949fd6b68553dedaceb0b03
bowersjuan <bowersjuan@gmail.com>
2023-01-06 18:30:11 -0500

    Add second logo and update Navbar New Link

--- src/Common/BudgetNav.js
+++ src/Common/BudgetNav.js
@@ -1,7 +1,9 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 import logo from "../budget-buddy-logo.png";
+import logo2 from "../budget-buddy-logo-2.png";
 import "./BudgetNav.css";
 
 export default function BudgetNav() {
@@ -10,13 +12,14 @@ export default function BudgetNav() {
       <Container>
         <Navbar.Brand href="">
           <img width="50em" src={logo} alt="logo" />
+          <img width="50em" src={logo2} alt="logo2" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/transactions">Transactions</Nav.Link>
-            <Nav.Link href="/transactions/new">New Transaction</Nav.Link>
+            <Nav.Link href="/transactions/new">New</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
