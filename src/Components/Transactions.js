a0135abfb0c2f5cbe31fec731cf3d68d1eb236ed
bowersjuan <bowersjuan@gmail.com>
2023-01-06 13:17:24 -0500

    update Transactions component with dynamic account balance display

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -7,18 +7,29 @@ import "./Transactions.css";
 const API = process.env.REACT_APP_API_URL;
 
 export default function Transactions() {
-  const [transactions, setTransactions] = useState();
+  const [transactions, setTransactions] = useState([]);
+  const [accountTotal, setAccountTotal] = useState(0);
+
+  function calcTotal(transactions) {
+    const total = transactions.reduce((acc, { amount }) => {
+      return acc + amount;
+    }, 0);
+    setAccountTotal(total);
+  }
 
   useEffect(() => {
     axios
       .get(`${API}/transactions`)
-      .then((response) => setTransactions(response.data))
+      .then((response) => {
+        setTransactions(response.data);
+        calcTotal(response.data);
+      })
       .catch((err) => console.error(err));
   }, []);
 
   return (
     <div className="Transactions">
-      <h4>Bank Account Total: #</h4>
+      <h4>Bank Account Total: ${accountTotal}</h4>
       <table>
         <thead>
           <tr>
