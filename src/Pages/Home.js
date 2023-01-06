cf189f34348e83356f9ccf486c392ed456cef67d
bowersjuan <bowersjuan@gmail.com>
2023-01-06 18:06:39 -0500

    update index and home pages

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="home">
+    <div>
       <h2>Home Page</h2>
     </div>
   );
--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -2,8 +2,7 @@ import Transactions from "../Components/Transactions";
 
 export default function Index() {
   return (
-    <div className="index">
-      <h2>Transactions</h2>
+    <div>
       <Transactions />
     </div>
   );
