b589bf3eeb0bd39f8b881ff48cafe3826261a49d
bowersjuan <bowersjuan@gmail.com>
2023-01-07 16:54:00 -0500

    update Transaction component styling

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
+import "./Transaction.css";
 
 export default function Transaction({ transaction }) {
+  const { id, amount, itemName, date } = transaction;
+
   return (
     <tr>
-      <th>{transaction.date}</th>
+      <th>{date}</th>
       <th>
-        <Link to={`/transactions/${transaction.id}`}>
-          {transaction.itemName}
+        <Link to={`/transactions/${id}`} style={{ textDecorationLine: "none" }}>
+          {itemName}
         </Link>
       </th>
-      <th>{transaction.amount}</th>
+      <th style={{ backgroundColor: amount > 0 ? "lime" : "red" }}>{amount}</th>
     </tr>
   );
 }
