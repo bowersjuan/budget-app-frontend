4e2b020bb4295e0386427e67467fce219ee95b01
bowersjuan <bowersjuan@gmail.com>
2023-01-09 12:08:33 -0500

    Add edit and delete button access to Index page

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -5,7 +5,7 @@ export default function Transaction({ transaction }) {
   const { id, amount, itemName, date } = transaction;
 
   return (
-    <tr>
+    <tr className="transaction-row">
       <th>{date}</th>
       <th>
         <Link to={`/transactions/${id}`} style={{ textDecorationLine: "none" }}>
@@ -13,6 +13,13 @@ export default function Transaction({ transaction }) {
         </Link>
       </th>
       <th style={{ backgroundColor: amount > 0 ? "lime" : "red" }}>{amount}</th>
+      {/* <th style={{ border: "none" }}></th> */}
+      <th style={{ border: "none" }}>
+        <button className="xxsmall-text">Edit</button>
+      </th>
+      <th style={{ border: "none" }}>
+        <button className="xxsmall-text">Delete</button>
+      </th>
     </tr>
   );
 }
