1044f917c798dc51bb4f20b25a590ccfc891bd5b
bowersjuan <bowersjuan@gmail.com>
2023-01-10 16:58:55 -0500

    add sound functionality to TransactialDetails component to show one-up sound for mario and fireball sound for bowser

--- src/Components/TransactionDetails.js
+++ src/Components/TransactionDetails.js
@@ -5,6 +5,8 @@ import EditButton from "./EditButton";
 import DeleteButton from "./DeleteButton";
 import mario from "../assets/mario.png";
 import bowser from "../assets/bowser.png";
+import oneUpSound from "../assets/sounds/smb_1-up-copy.wav";
+import fireballSound from "../assets/sounds/smb_bowserfire-copy.wav";
 import { formatDate } from "../helpers/dateFormatters";
 import "./TransactionDetails.css";
 
@@ -25,71 +27,76 @@ export default function TransactionDetails() {
       .catch((err) => console.error(err));
   }, [id]);
 
-  return (
+  return transaction ? (
     <div className="transaction-details">
-      <section className="empty"></section>
+      <section className="empty">
+        <audio autoPlay>
+          <source
+            src={transaction.amount > 0 ? oneUpSound : fireballSound}
+            type="audio/wav"
+          />
+          <track src="" kind="captions" srcLang="en" label="English" />
+          Your browser does not support the audio element.
+        </audio>
+      </section>
       <section
         className="data-section"
         style={{
-          backgroundColor: transactionColorPicker(transaction?.amount),
+          backgroundColor: transactionColorPicker(transaction.amount),
         }}>
         <p className="white-bkgrd" style={{ borderBottom: "5px solid black" }}>
           Date
         </p>
-        {transaction ? <p>{formatDate(transaction.date)}</p> : null}
+        <p>{formatDate(transaction.date)}</p>
       </section>
       <section className="empty"></section>
       <section
         className="data-section"
         style={{
-          backgroundColor: transactionColorPicker(transaction?.amount),
+          backgroundColor: transactionColorPicker(transaction.amount),
         }}>
         <p className="white-bkgrd" style={{ borderBottom: "5px solid black" }}>
           Name
         </p>
-        <p>{transaction?.itemName}</p>
+        <p>{transaction.itemName}</p>
       </section>
       <section
         className="data-section"
         style={{
-          backgroundColor: transactionColorPicker(transaction?.amount),
+          backgroundColor: transactionColorPicker(transaction.amount),
         }}>
-        {transaction ? (
-          <>
-            <img
-              style={{ padding: "10px" }}
-              width="60px"
-              src={transaction.amount > 0 ? mario : bowser}
-              alt={transaction.amount > 0 ? "mario" : "bowser"}
-            />
-            <p style={{ fontSize: "medium" }}>{transaction.amount}</p>
-          </>
-        ) : (
-          ""
-        )}
+        <>
+          <img
+            style={{ padding: "10px" }}
+            width="60px"
+            src={transaction.amount > 0 ? mario : bowser}
+            alt={transaction.amount > 0 ? "mario" : "bowser"}
+          />
+          <p style={{ fontSize: "medium" }}>{transaction.amount}</p>
+        </>
       </section>
       <section
         className="data-section"
         style={{
-          backgroundColor: transactionColorPicker(transaction?.amount),
+          backgroundColor: transactionColorPicker(transaction.amount),
         }}>
         <p className="white-bkgrd" style={{ borderBottom: "5px solid black" }}>
           Category
         </p>
-        <p>{transaction?.category}</p>
+        <p>{transaction.category}</p>
       </section>
       <EditButton id={id} />
       <section
         className="data-section"
         style={{
-          backgroundColor: transactionColorPicker(transaction?.amount),
+          backgroundColor: transactionColorPicker(transaction.amount),
         }}>
         <p className="white-bkgrd" style={{ borderBottom: "5px solid black" }}>
           From
         </p>
-        <p>{transaction?.from}</p>
+        <p>{transaction.from}</p>
       </section>
       <DeleteButton id={id} />
     </div>
-  );
+  ) : null;
 }
