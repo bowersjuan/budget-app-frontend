379e35beb7113cd1e62a5a3cd76df622ea34dff5
bowersjuan <bowersjuan@gmail.com>
2023-01-10 02:25:12 -0500

    update Home page with mario coin block gif

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,22 @@ export default function Home() {
     <div>
       <h2>Budget Buddy</h2>
       <p>Your digital wealth manager</p>
-      <div>💰🤖</div>
+      <div
+        style={{
+          width: "100%",
+          height: "0",
+          paddingBottom: "111%",
+          position: "relative",
+        }}>
+        <iframe
+          title="mario-coin-block-gif"
+          src="https://giphy.com/embed/S5uMJDmtnATLbjjw3h"
+          width="432"
+          height="200"
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen></iframe>
+      </div>
     </div>
   );
 }
