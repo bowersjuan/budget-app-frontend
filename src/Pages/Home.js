b1812e3f25e6f8b414ad5980cca1bb8be7b69ae8
bowersjuan <bowersjuan@gmail.com>
2023-01-11 01:32:13 -0500

    update Home component with budget-buddy-logo-2 image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
+import logo2 from "../assets/budget-buddy-logo-2.png";
 import Gif from "../Components/Gif";
 
 export default function Home() {
   return (
     <div>
-      <h2>Budget Buddy</h2>
+      <img width="200px" style={{ resize: "0px" }} src={logo2} alt="logo2" />
       <p>Your digital wealth manager</p>
       <Gif link="https://giphy.com/embed/S5uMJDmtnATLbjjw3h" />
     </div>
